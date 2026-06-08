e251c1bc3c01cb64d25ce5cfb25425aafb4ad6ee
Ibai Irastorza <batxes@gmail.com>
2026-06-08 21:37:39 +0200

    Connect Supabase

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // Fill these public values after creating the Supabase project.
 // Keeping both empty enables the browser-only demo mode.
 window.PORRA_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  // Emails that should be treated as site administrators.
+  supabaseUrl: "https://dngpzidlijyntllnaxax.supabase.co",
+  supabaseAnonKey: "sb_publishable_05tIYCIKK0LNw5YGMSLl8g_lSRNa6K5",
   adminEmails: ["admin@admin.admin"],
 };
