1907bd2e03335942a29208c7a2d958158dbcc3e0
Hodei <quantum@quantumlawyer.io>
2026-06-30 17:26:24 +0200

    Ease Ronaldao risk curve

--- src/components/ronaldao-limbo-modal.tsx
+++ src/components/ronaldao-limbo-modal.tsx
@@ -61,7 +61,7 @@ const MAX_ROUNDS = 3;
 const DROP_MS = 860;
 const FEED_MS = 520;
 const EXPLOSION_MS = 980;
-const RISK_STEPS = [12, 22, 35, 50, 65, 80];
+const RISK_STEPS = [9, 15, 25, 38, 52, 68];
 
 const DEFAULT_REWARDS: RonaldaoLimboReward[] = [
   { image: "/sobre-defensas.webp", pool: "defensas", title: "Sobre Defensas" },
